b2f8c23196bf772557f3e4d49050e8e1664ba462
itsmealessandro <itsmealessandro@outlook.com>
2023-05-06 19:48:12 +0200

    Creazione vista e controller della gestione profilo

---
+++ Initech Application/src/main/java/controllers/GestioneProfiloController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import domain.Utente;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GestioneProfiloController implements DataInitializable<Utente>, Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void initializeData(Utente utente) {
+
+    }
+}
