df437acc67598ec97a416a1168a76b8fb748e70c
itsmealessandro <itsmealessandro@outlook.com>
2023-05-16 17:55:26 +0200

    [Update] Implementazione gestione-soci vista, controller e logica di Business

--- Initech Application/src/main/java/business/UtenteService.java
+++ Initech Application/src/main/java/business/UtenteService.java
@@ -1,16 +1,21 @@
 package business;
 
+import domain.Socio;
 import domain.Utente;
 import domain.UtenteRegistrato;
+
+import java.util.List;
 
 public interface UtenteService {
 
     Utente authenticate(String username, String password) throws UtenteNotFoundException, BusinessException;
-    boolean registrazione(UtenteRegistrato utente) throws BusinessException;
 
+    boolean registrazione(UtenteRegistrato utente) throws BusinessException;
 
     void rimuoviUtente(int id) throws BusinessException;
 
-    boolean modificaUtente(Utente utente,String newUsername) throws BusinessException;
+    boolean modificaUtente(Utente utente, String newUsername) throws BusinessException;
+
+    List<Socio> getAllSoci() throws BusinessException;
 
 }
--- Initech Application/src/main/java/controllers/GestioneSociController.java
+++ Initech Application/src/main/java/controllers/GestioneSociController.java
@@ -4,20 +4,19 @@ import business.BusinessException;
 import business.InitechBusinessFactory;
 import business.UtenteService;
 import domain.Socio;
+import domain.TipologiaSocio;
 import domain.Utente;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import view.ViewDispatcher;
 
 import java.net.URL;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class GestioneSociController implements Initializable, DataInitializable<Utente> {
@@ -28,15 +27,13 @@ public class GestioneSociController implements Initializable, DataInitializable<
     @FXML
     TableView sociTable;
     @FXML
-    TableColumn<String, Socio> nomeColumn;
+    TableColumn<Socio, String> nomeColumn;
     @FXML
-    TableColumn<String, Socio> cognomeColumn;
+    TableColumn<Socio, String> cognomeColumn;
     @FXML
-    TableColumn<String, Socio> tipologiaColumn;
+    TableColumn<Socio, TipologiaSocio> tipologiaColumn;
     @FXML
-    TableColumn<String, Socio> emailColumn;
-    @FXML
-    TableColumn<String, Button> azioneColumn;
+    TableColumn<Socio, String> emailColumn;
     @FXML
     Label errorLabel;
 
@@ -50,12 +47,32 @@ public class GestioneSociController implements Initializable, DataInitializable<
         errorLabel.setVisible(false);
 
 
+        nomeColumn.setStyle("-fx-alignment: CENTER");
+        nomeColumn.setCellValueFactory(new PropertyValueFactory<>("nome"));
+
+        cognomeColumn.setStyle("-fx-alignment: CENTER");
+        cognomeColumn.setCellValueFactory(new PropertyValueFactory<>("cognome"));
+
+        tipologiaColumn.setStyle("-fx-alignment: CENTER");
+        tipologiaColumn.setCellValueFactory(new PropertyValueFactory<>("tipologiaSocio"));
+
+        emailColumn.setStyle("-fx-alignment: CENTER");
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+
     }
 
     @Override
     public void initializeData(Utente utente) {
 
-       
+        try {
+            ObservableList<Socio> socioObservableList = FXCollections.observableList(utenteService.getAllSoci());
+            sociTable.setItems(socioObservableList);
+
+        } catch (BusinessException e) {
+            errorLabel.setText(e.getMessage());
+            errorLabel.setVisible(true);
+        }
 
 
     }
